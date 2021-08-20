dc8c42921f4f84609f1d09a39d2ee60816b4a716
DDiaz <daniel.diaz.0515@gmail.com>
2021-08-20 10:13:30 -0700

    Functions update: passLen. NumConfirm. SpecConfirm

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passLength = '';
 
-//I want to declare the constant values I will be working with//
+//Clobal Variable for writePassword Function//
+var passLength = '';
+var numberConfirm = '';
+var characterConfirm = '';
+
+
+//Arrays for password selection//
     
     //upper and lowe case characters//
     var characterUplow = ['a','b','c','d','e','f','g','h','i','j','k','k','l','m','n','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -19,50 +24,68 @@ var passLength = '';
             console.log(specialCharacter);
 
 
-//When generateBtn clicked I want to prompt the user asking how long they want their passord//
-    //min 8, max128//
 
-        //I want to store this number and turn it into the length of an array with a variable name pswdLength//
+//Password Length Function//
+function lengthInput (){
+    
+    //Prompts User for Password Length
+    var x = window.prompt('Your password can contain between 8-128 characters: How long do you want your password?', '8-128');
+    //Turns Password Length from String to Num//
+    pL = parseInt(x);
 
+    //Confirms Type of Int//
+    console.log(typeof pL);
 
+    //Verifying parameters of password length//
 
-// User is promted and confirms/ rejects upper and lower case//
-
-    //Confirming stores Bool as true//
-    //Rejecting stores Bool as false//
-
-        // I want to store this Bool//
-
-            // True or false will determine if the upper/lowercase array will apply//
-
-//User is prompted and confirms/rejects numeric characters//
-
-    //Confirming stores Bool as true//
-    //Rejecting stores Bool as false//
-
-        //I want to store this Bool//
-
-            //True or false will determine if the numeric characters array will apply//
+    if(pL >= 8 || pL >= 128){
+             
+            console.log(pL);
+            console.log(typeof pL);
+            passLength = pL;
+            console.log(passLength);
+            console.log(typeof passLength);
 
 
-
-//User is prompted and confirms/rejects special characters//
-
-    //Confirming stores Bool as true//
-    //Rejecting stores Bool as false//
-
-        //I want to store this Bool//
-
-            //True or False will determine if the special characters will apply//
+    }else if (pL < 8 || pL > 128){
+        alert("You need to choose a number between 8-128. Press Generate passwork again to try again");
+    }
+}
 
 
-// Confirm all selected parameters//
+//Numbers in Password Function//
 
-//Combine all applicaple arrays together//
+function numberInclude(){
 
-//Randomly select from arrays until specified length given by user is reached//
+    var n =window.confirm("Would you like to include numbers in your password?");
+
+    console.log(n);
+    console.log(typeof n);
+
+    numberConfirm = n;
+
+    console.log(numberConfirm);
+    console.log(typeof numberConfirm);
 
 
+}
+
+
+//Special Characters in Password Function//
+function characterInclude(){
+
+    var s =window.confirm("Would you like to include special characters in your password?");
+
+    console.log(s);
+    console.log(typeof s);
+
+    characterConfirm = s;
+
+    console.log(characterConfirm);
+    console.log(typeof characterConfirm);
+
+
+}
 
 
 // Write password to the #password input//
@@ -71,33 +94,27 @@ function writePassword(event) {
     event.preventDefault();
 
     //Length of Password//
-
-        //When generateBtn clicked I want to prompt the user asking how long they want their passord//
-            var x = window.prompt('Your password can contain between 8-128 characters: How long do you want your password?', '8-128');
-            pL = parseInt(x);
-            console.log(typeof pL);
-            //min 8, max128//
-
-                //Creating Array to specified length
-
-                if(pL >= 8 || pL >= 128){
-                         
-                        console.log(pL);
-                        console.log(typeof pL);
-                        passLength = pL;
-                        console.log(passLength);
-                        console.log(typeof passLength);
-
-
-                }else if (pL < 8 || pL > 128){
-                    alert("You need to choose a number between 8-128. Press Generate passwork again to try again");
-                }
+     lengthInput();
+    //Number Included or Not//
+     numberInclude();
+    //Special Char. Included or Not//
+    characterInclude();
+           
             
-                        
-
-                //I want to store this number and turn it into the length of an array with a variable name pswdLength//
 
   var password = generatePassword();
+
+    //I want to produce a password//
+
+    //I want to identify arrays to be used//
+
+    //I want to select at random one item from each array and put them into a new array//
+
+    //I want to then select a item from each specified array and push them into new array 'n' times until new array is equal to passLength//
+
+    //I want to take the new array and print text into aria-label box//
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
